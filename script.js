14f6c5667577ac405332d9a32a5f44ee29bae82d
shannonmtierney-sudo <shannonmtierney@lewisu.edu>
2026-03-03 04:17:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript Enhancement #1
+document.getElementById("myName").onclick = function() {
+    this.style.color = "pink";
+};
+
+// JavaScript Enhancement #2
+function showDate() {
+    document.getElementById("dateOutput").innerHTML = new Date();
+}
